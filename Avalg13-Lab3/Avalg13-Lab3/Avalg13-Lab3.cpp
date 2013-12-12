5d04a0987f191ac211f0a66280f27a84c07a97ef
fericss <fericss@gmail.com>
2013-12-12 18:27:03 +0100

    Uploading base

---
+++ Avalg13-Lab3/Avalg13-Lab3/Avalg13-Lab3.cpp
@@ -0,0 +1,13 @@
+// Avalg13-Lab3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	return 0;
+}
+
