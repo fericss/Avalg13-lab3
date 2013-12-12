c6737d54b8323e16727597599437be0df78fa796
fericss <fericss@gmail.com>
2013-12-12 23:46:45 +0100

    Game core features implemented

--- Avalg13-Lab3/Avalg13-Lab3/Avalg13-Lab3.cpp
+++ Avalg13-Lab3/Avalg13-Lab3/Avalg13-Lab3.cpp
@@ -2,32 +2,52 @@
 //
 
 #include "stdafx.h"
+#include "Avalg13-Lab3.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <functional>
 
 using namespace std;
 
 
+Game g;
+string game_start_intro = "Bla bla bla, Bloooooo\nTo view a list of commands type: list";
 
-
-void quitGame(){
-	exit(0);
-}
 
 struct
 {
-	(void)(*fn)();
+	(void)(Game::*fn)(const vector<string> & args);
 	const char* key;
 }
 function_lookup_table[] =
 {
-	{ &quitGame, "quit" },
-	{ NULL, NULL }
+	{ &Game::quitGame,"quit"},
+	{ &Game::listCommands, "list" }
 };
-bool lookup_and_call(const string & name){
+
+
+
+
+
+void Game::listCommands(const vector<string> & args){
+	for each (auto var in function_lookup_table)
+	{
+		cout << var.key << endl;
+	}
+}
+
+void Game::quitGame(const vector<string> & args){
+
+
+	exit(0);
+}
+
+
+bool Game::lookup_and_call(const vector<string> & args){
 	for (int i = 0; function_lookup_table[i].fn; i++){
-		if (name == function_lookup_table[i].key){
-			(*(function_lookup_table[i].fn))();
+		if (args[0] == function_lookup_table[i].key){
+			(g.*(function_lookup_table[i].fn))(args);
 			return true;
 		}
 	}
@@ -35,16 +55,22 @@ bool lookup_and_call(const string & name){
 }
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, _TCHAR* argv[])
 {
+	g = Game();
 	string command;
-	
-	while (true){
-		getline(cin, command);
-		if (lookup_and_call(command)){
 
+	cout << game_start_intro << endl;
+
+	vector<string> args = vector<string>();
+	while (true){
+		cout << ">>";
+		getline(cin, command);
+		args.clear();
+		for (size_t p = 0, q = 0; p != command.npos; p = q){
+			args.push_back(command.substr(p + (p != 0), (q = command.find(' ', p + 1)) - p - (p != 0)));
 		}
-		else{
+		if (!g.lookup_and_call(args)){
 			cout << "Unknown command: " << command << endl;
 		}
 		
